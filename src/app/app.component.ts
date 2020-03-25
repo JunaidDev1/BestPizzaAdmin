ef204fb0f582314f5349fac58f45e1a8120cbc00
JunaidDev1 <51758839+JunaidDev1@users.noreply.github.com>
2020-03-25 23:37:11 +0500

    Login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'BestPizzaAdmin';
+
+  onActivate(event) {
+    window.scroll(0, 0);
+  }
 }
